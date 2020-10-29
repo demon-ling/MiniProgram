642d2c2b19e8f06f4b9f8e27803b54043c9ba23e
凌联勇 <1619515295@qq.com>
2020-10-29 17:00:32 +0800

    知识点1

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,62 +5,24 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    name:"demon-ling",
+    age:20,
+    students:[
+      {id:112,name:'kobe',age:30},
+      {id:115,name:'sily',age:32},
+      {id:111,name:'jams',age:29}
+    ],
+    counter:0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleBtnClick(){
+    // 调用this.setData()
+    this.setData({
+      counter:this.data.counter += 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleSubtraction(){
+    this.setData({
+      counter:this.data.counter -=1
+    })
   }
 })
